a1ebedb85efad1cb397a3be7bff908d2cc8fe756
Jackeagle <jackeagle102@gmail.com>
2015-02-20 03:01:03 -0500

    Gladify Wallpaper App Completly
    Change-Id: Ieb4248fa6adf77abee5a0e9539280aeb8d2c1947
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/glade/wallpapers/BitmapCropTask.java
+++ src/com/glade/wallpapers/BitmapCropTask.java
@@ -1,4 +1,4 @@
-package com.cyanogenmod.wallpapers;
+package com.glade.wallpapers;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
--- src/com/glade/wallpapers/wallpaper.java
+++ src/com/glade/wallpapers/wallpaper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.wallpapers;
+package com.glade.wallpapers;
 
 import android.app.Activity;
 import android.app.WallpaperManager;
